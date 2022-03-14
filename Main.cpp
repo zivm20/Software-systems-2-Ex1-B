9e65560dfb0d49cf872a731256e2b0a03497971a
zivm20 <ziv.morgan@gmail.com>
2022-03-14 21:02:32 +0200

    added comments

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,10 @@
 #include <iostream>
 #include <stdexcept>
 using namespace std;
-
+/**
+ * Takes string and splits it by the deliminator 
+ *
+ */
 vector<string> split(string str, string deliminator){
 	int start = 0;
 	int end = 0;
@@ -30,15 +33,20 @@ vector<string> split(string str, string deliminator){
 int main() {
 
 	cout << "Enter desired values (seperated by spaces) for all mats (split by 2 spaces) and then press enter"<<endl<<"format: width height char1 char2  width height char1 char2"<<endl;
-	
+	//get entire line input
 	string line;
 	getline(cin,line,'\n');
+
+	//each mat is seperated by "  "
 	vector<string> mats = split(line,"  ");
 	for(string s: mats){
+		//get the 4 args in each mat
 		vector<string> args = split(s," ");
+		//if more than 4 args were given, only use the first 4 and continue
 		if(args.size() >= 4){
 			int width, height;
 			char c1, c2;
+			//if the user gave us invalid args, we continue to the next mat
 			try{
 				width = stoi(args[0]);
 				height = stoi(args[1]);
@@ -51,8 +59,7 @@ int main() {
 			}
 		}
 	}
-
-
+	return 0;
 }
 
 
